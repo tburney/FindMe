e88bcdb9ad561910a171e30a8ac02839ed03e6c9
tburney <tburney@users.noreply.github.com>
2015-03-25 14:41:29 +0000

    Project boilerplate with express

---
+++ tests/tests.js
@@ -0,0 +1,10 @@
+/**
+ * Created by timon on 24/03/15.
+ */
+var should = require('should');
+var assert = require('assert');
+describe('Test Framework', function () {
+    it('should have mocha installed and running.', function () {
+        assert.equal(true, true);
+    })
+})
